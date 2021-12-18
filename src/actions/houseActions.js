19951bebfa56a2d572820fa7cb387cb6b808ed2f
forhadakbar <forhadakbar@yahoo.com>
2021-12-18 00:18:50 -0500

    first commit

---
+++ src/actions/houseActions.js
@@ -0,0 +1,32 @@
+
+import axios from 'axios'
+import {
+  HOUSE_LIST_REQUEST,
+  HOUSE_LIST_SUCCESS,
+  HOUSE_LIST_FAIL
+} from '../constants/houseConstants'
+
+export const listHouses = () => async (
+  dispatch
+) => {
+  try {
+    dispatch({ type: HOUSE_LIST_REQUEST })
+
+    const { data } = await axios.get(
+      `https://rocky-shelf-02632.herokuapp.com/apartments`
+    )
+
+    dispatch({
+      type: HOUSE_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: HOUSE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
